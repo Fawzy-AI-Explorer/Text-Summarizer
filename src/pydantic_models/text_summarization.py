308a10f309e959ce1c9c3cf9d91e6f5f9fc10ece
Mohammad Fawzy <moha.fawzy63@gmail.com>
2025-06-22 11:50:05 +0300

    add Pydantic model for text summarization requests

---
+++ src/pydantic_models/text_summarization.py
@@ -0,0 +1,16 @@
+"""Pydantic models for text summarization tasks."""
+
+from pydantic import BaseModel, Field
+
+class TextSummarization(BaseModel):
+    """Request model for text summarization.
+    Attributes:
+        summarized_text (str): The text to be summarized.
+    """
+
+    summarized_text: str = Field(
+        ...,
+        description="The text to be summarized.",
+        max_length=300,
+        min_length=10
+    )
